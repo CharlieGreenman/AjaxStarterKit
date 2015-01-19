68f2a4c294d80aea5dab0595f6b3dc8527eae1ec
CharlieGreenman <charlie@Charlies-MacBook-Pro.local>
2015-01-18 19:31:57 -0500

    changed things up a bit

--- js/canvas.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded',domloaded,false);
-function domloaded(){
-    var c = document.getElementById('canvas');
-    var ctx = c.getContext('2d');
-
-    ctx.fillStyle = 'f00';
-    ctx.fillRect(100,100,300,400);
-
-}
